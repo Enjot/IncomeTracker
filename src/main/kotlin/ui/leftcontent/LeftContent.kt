1322054ee9cf0222b062130cddda8893a2e79cd0
Enjot <jakubnahacz@gmail.com>
2023-09-14 16:39:11 +0200

    minor changes

--- src/main/kotlin/ui/leftcontent/LeftContent.kt
+++ src/main/kotlin/ui/leftcontent/LeftContent.kt
@@ -1,15 +1,10 @@
 package ui.leftcontent
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
