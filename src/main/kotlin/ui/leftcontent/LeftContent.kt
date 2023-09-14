4a5e5ea42c044d058176d644f32b8b7eb5c844b2
Enjot <jakubnahacz@gmail.com>
2023-09-14 15:23:05 +0200

    minor changes

--- src/main/kotlin/ui/leftcontent/LeftContent.kt
+++ src/main/kotlin/ui/leftcontent/LeftContent.kt
@@ -1,10 +1,15 @@
 package ui.leftcontent
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -17,7 +22,16 @@ fun LeftContent(
         modifier
             .padding(24.dp)
     ) {
+        Icon(
+            painter = painterResource("drawable/picture_as_pdf_FILL1_wght400_GRAD0_opsz24.svg"),
+            contentDescription = null,
+            tint = Color.Black,
+            modifier = Modifier
+                .size(48.dp)
+                .clickable{
 
+                }
+        )
         Month()
 
         SortingButton(
