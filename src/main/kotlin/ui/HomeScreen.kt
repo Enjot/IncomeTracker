e5d2ff4674d10a648157269e00d16c8bedd70f69
Enjot <jakubnahacz@gmail.com>
2023-10-12 17:22:52 +0200

    HomeScreen.kt adjusted white spaces

--- src/main/kotlin/ui/HomeScreen.kt
+++ src/main/kotlin/ui/HomeScreen.kt
@@ -12,34 +12,34 @@ import ui.rightcontent.RightContent
 
 // root composable function
 
-    @Composable
-    fun HomeScreen(
-        screenModel: HomeScreenModel,
-        modifier: Modifier = Modifier
+@Composable
+fun HomeScreen(
+    screenModel: HomeScreenModel,
+    modifier: Modifier = Modifier
+) {
+
+    val spendings = screenModel.spendings.collectAsState(emptyList())
+
+    // blank surface that fill whole window and change color itself depending on theme
+    Surface(
+        color = MaterialTheme.colors.surface,
+        modifier = modifier
     ) {
-
-        val spendings = screenModel.spendings.collectAsState(emptyList())
-
-        // blank surface that fill whole window and change color itself depending on theme
-        Surface(
-            color = MaterialTheme.colors.surface,
-            modifier = modifier
-        ) {
-            // inside main window we made two separated blocks to handle interface
-            Row() {
-                LeftContent(
-                    onAddClick = { name, amount -> screenModel.insertSpending(name, amount) },
-                    onDeleteClick = { id -> screenModel.deleteSpending(id) },
-                    items = spendings.value,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .weight(1f)
-                )
-                RightContent(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .weight(1f)
-                )
-            }
+        // inside main window we made two separated blocks to handle interface
+        Row() {
+            LeftContent(
+                onAddClick = { name, amount -> screenModel.insertSpending(name, amount) },
+                onDeleteClick = { id -> screenModel.deleteSpending(id) },
+                items = spendings.value,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            )
+            RightContent(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            )
         }
     }
+}
