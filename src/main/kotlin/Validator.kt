cb66a733d3ad9d2e2cf262fbbedc68b4b3556030
Enjot <jakubnahacz@gmail.com>
2023-11-16 15:15:51 +0100

    minor changes

--- src/main/kotlin/Validator.kt
+++ src/main/kotlin/Validator.kt
@@ -1,22 +1,23 @@
-class Validator {
-    companion object {
-        fun amountOfMoney(enteredValue: String): Boolean {
-            return try {
-                enteredValue.toDouble()
-                true
-            } catch (_: Exception) {
-                false
-            }
-        }
+object Validator {
 
-        fun quantityOf(enteredValue: String): Boolean {
-            return try {
-                enteredValue.toInt()
-                true
-            } catch (_: Exception) {
-                false
-            }
+    fun amountOfMoney(enteredValue: String): Boolean {
+        return try {
+            enteredValue.toDouble()
+            true
+        } catch (_: Exception) {
+            false
         }
     }
+
+    fun quantityOf(enteredValue: String): Boolean {
+        return try {
+            enteredValue.toInt()
+            true
+        } catch (_: Exception) {
+            false
+        }
+    }
+    
+    
 }
 
